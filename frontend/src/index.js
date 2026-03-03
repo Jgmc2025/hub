511aada222189f59b90aea38954edecd9ede8c79
João Gabriel Medeiros Caldas <joao.caldas@creatorsfy.com>
2026-03-03 05:59:51 -0300

    fix: revert-css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './Routes'; 
 
